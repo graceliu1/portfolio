a4b30f22bc4f27cbf885f6327da2951d99dda633
Grace Liu <gl2969@princeton.edu>
2023-12-05 23:08:36 -0500

    another change

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,31 @@
 module.exports = {
   siteMetadata: {
     title: `personal_portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.grace-liu.netlify.app`
   },
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image",
-  "gatsby-plugin-sharp"]
+  "gatsby-plugin-sharp", 
+      {
+        resolve: `gatsby-omni-font-loader`,
+        options: {
+          enableListener: true,
+          preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+          web: [
+            {
+              name: `Open Sans`,
+              file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+            }
+            ,
+            {
+              name: `Esteban`,
+              file: `https://fonts.googleapis.com/css2?family=Esteban&display=swap`
+            },
+            {
+              name:`Cormorant Garamond`,
+              file:`https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Esteban&display=swap`
+            }
+          ],
+        },
+      },
+]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ const pageStyles = {
   backgroundColor: "#FBF7F4", // 
   color: "#232129",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: "Open Sans, -apple-system, Roboto, sans-serif, serif",
 }
 const pageStylesWidth = {
   maxHeight: 1200
@@ -23,6 +23,9 @@ const listItemStyles = {
 }
 const sublistItemStyles = {
   marginLeft: 15,
+}
+const poemStyles = {
+  fontFamily: "Cormorant Garamond"
 }
 
 const links = [
@@ -49,9 +52,11 @@ const links = [
 
       <button class="inline-block bg-rose-700/10 duration-150 hover:bg-rose-700/30 text-4xl p-2"><code>{'>'}{'>'} Grace Liu ٩(＾◡＾)۶</code></button>
       
-      <span class="text-lg ml-5 inline-block float-right"><code>{'<'}{'<'}<span class="underline"> say hi </span>  </code></span>
-      <span class="text-lg ml-5 inline-block float-right"><code>{'<'}{'<'} <span class="underline"> resume </span></code></span>
-      <span class="text-lg ml-5 inline-block float-right"><code>{'<'}{'<'} <span class="underline"> <a href = "#projects">projects</a> </span> </code></span>
+      <span class="text-lg ml-5 inline-block float-right"><code>{'<'}{'<'} <span class="hover:text-slate-500" > <a href="#socials">say hi</a> </span>  </code></span>
+      <span class="text-lg ml-5 inline-block float-right"><code>{'<'}{'<'} <span class="hover:text-slate-500">
+         <a target="_blank" href = "https://docs.google.com/document/d/e/2PACX-1vRAt12DnJGuIbfaXXOlBqrXSmV_1KPa5FcnRl9ATbbP1MF-kABKLHEQ0HimJVqUQGUhXKLH174g6MuT/pub"> resume </a>
+         </span></code></span>
+      <span class="text-lg ml-5 inline-block float-right"><code>{'<'}{'<'} <span class="hover:text-slate-500"> <a href = "#projects">projects</a> </span> </code></span>
      
      
       
@@ -61,12 +66,12 @@ const links = [
      
       <button class="bg-[#e3a39b] my-3 duration-150 hover:bg-red-700/70  p-1"><code>CS @ Princeton | Class of 2026 | gl2969@princeton.edu </code></button>
       <br />
-      <p class="my-4 w-5/6">Hello! I'm a curious and enthusiastic student interested in <b>Software Engineering</b> and <b>Quantitative Trading</b> roles.
-      I'm studying Computer Science with intended minors in Statistics and Machine Learning and Quantitative Economics. 
-      On campus, I'm involved with acapella, HackPrinceton, SWE, and Princeton Women in Computer Science.
+      <p class="my-4 w-5/6" >Hello! I'm a curious and enthusiastic student interested in <b>Software Engineering</b> and <b>Quantitative Trading</b> roles.
+      I'm studying Computer Science with intended minors in Statistics and Machine Learning & Quantitative Economics. 
+      On campus, I'm involved with Old NasSoul a-capella, HackPrinceton, SWE, and Princeton Women in Computer Science.
       <br /> <br />
-      I grew up in the Bay Area and worked on large language model chatbots last summer as a Software Engineering Intern at Cadence Design Systems.
-       Some of my favorite things include: word games (puzzles of all sorts, really), video games, and music. I also dabble in poetry. 
+      I grew up in the Bay Area and interned there last summer at Cadence Design Systems. Some of my favorite pastimes are word games (puzzles of all sorts, really), video games, and music. I also dabble in poetry, which
+      you can check out on this page.
        I'm always looking to find creative ways to solve new problems :).</p>
       <br/>
   
@@ -97,7 +102,7 @@ const links = [
         </p>
         </div>
        </div>
-       <div class="border-2 border-black duration-150 bg-[#a66f41]/70 hover:bg-[#a66f41]/90 overflow-hidden shadow-md">
+       <div class="border-2 border-black duration-150 bg-[#ab6930]/60 hover:bg-[#a66f41]/90 overflow-hidden shadow-md">
         <div class="px-12 py-10">
        <button class=" text-lg mb-2 px-1 py-0.5"><code>experience ࿐ ࿔*:･ﾟ</code></button>
 
@@ -120,7 +125,7 @@ const links = [
     
     <section id = "projects" class="bg-[#e3a39b] duration-150 hover:bg-[#e3a39b]/100 border-2 border-black  overflow-hidden shadow-md">
         <div class="px-12 py-10">
-        <button class="text-lg mt-2 mb-2 px-1 py-0.5"><code>projects ⋆ ˚｡⋆୨୧˚</code></button>
+        <button class="text-lg mt-2 mb-2 px-1 py-0.5"><code>resume ⋆ ˚｡⋆୨୧˚</code></button>
         <p class="my-4">
           <ul>
             <li style ={listItemStyles}><code>{'>'}{'>'}</code> <b>Product Safety from Text Recognition Application:</b></li>
@@ -135,9 +140,9 @@ const links = [
     <br />
     
     <div class=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-7">
-      <div class="border-2 border-black duration-150 bg-[#a66f41]/70 hover:bg-[#a66f41]/90 overflow-hidden shadow-md">
+      <div class="border-2 border-black duration-150 bg-[#ab6930]/70 hover:bg-[#a66f41]/90 overflow-hidden shadow-md">
         <div class="px-12 py-10">
-       <button class=" text-lg mb-2 px-1 py-0.5"><code>eluo - product safety</code></button>
+       <button class=" text-lg mb-2 px-1 py-0.5"><code>PROJECT : eluo for product safety</code></button>
 
         <p class="my-4">
           <ul>
@@ -151,7 +156,7 @@ const links = [
 
       <div class="border-2 border-black bg-emerald-700/40 duration-150 hover:bg-emerald-700/60 overflow-hidden shadow-md">
         <div class="px-12 py-10">
-      <button class="text-lg mb-2 px-1 py-0.5"><code>dublin math course planner </code></button>
+      <button class="text-lg mb-2 px-1 py-0.5"><code>PROJECT : DHS math course planner </code></button>
         
         <p class="my-4">
           <ul>
@@ -169,60 +174,85 @@ const links = [
     <section id = "poetry" class="bg-[#e3a39b] duration-150 hover:bg-[#e3a39b]/100 border-2 border-black  overflow-hidden shadow-md">
         <div class="px-12 py-10">
         <button class="text-lg mt-2 mb-2 px-1 py-0.5"><code>poetry ⋆ ˚｡⋆୨୧˚</code></button>
-        <p class="my-4">
+        <section style = {poemStyles} class="bg-white border-dotted border-black border-2 p-8 my-4">
           
-familiar, not foreign<br></br>
-<b>i. <em>chu sheng</em> / beginning: </b>
-i am the daughter of two tigers.
-my mother rubs bitter spice between her fingers,
-her fortunes cradled in ginger embraces.
-she tells me of a Goddess’s flour-streaked hands;
-birthing <em>zong zi</em> children, fated to drown
-in riverboats of dried bamboo leaves.
-wistful, we recall forgotten stories from past summers: 
-          a Monkey King frolicking in the nectar,
-          cracked sunflower seeds sprouting new stalks,
-          smears of tiger balm that stain elbows forest-green.
-to my mother, the secrets of the world are 
-hidden in the altar of the Budda’s face
-and the smoke of the cloying incense.
-to me, they peek out in the curve of her crescent smile.
+        <div class = "underline mb-4">the foreigner<br /></div>
+          <b>i. <em>chu sheng</em> / beginning: </b>
+          i am the daughter of two tigers.
+          my mother rubs bitter spice between her fingers,
+          her fortunes cradled in ginger embraces.
+          she tells me of a Goddess’s flour-streaked hands;
+          birthing <em>zong zi</em> children, fated to drown
+          in riverboats of dried bamboo leaves.
+          wistful, we recall forgotten stories from past summers: 
+                    a Monkey King frolicking in the nectar,
+                    cracked sunflower seeds sprouting new stalks,
+                    smears of tiger balm that stain elbows forest-green.
+          to my mother, the secrets of the world are 
+          hidden in the altar of the Budda’s face
+          and the smoke of the cloying incense.
+          to me, they peek out from the curve of her crescent smile.
+          <br />
+          <br /><b>ii. <em>jie ju</em> / end: </b>
+          i am the lover of the half-melted mango sun.
+          but on the fullest moon i gulp down salted egg yolks
+          devour crushed sesame and lotus paste in the dark.
+          wistful, my grandfather hands me his jade elephants.
+          curls of  cigar smoke bade me farewell. 
+          my memories of this town are    superimposed through
+          the mirage of six months of my childhood.
+          how can i recognize home?
+          longing and reluctant to leave, i turn towards:
+                    the modern city of infinite western luxuries,
+                    a (still) too-tight <em>qi pao</em> discarded on the floor,
+                    the bloodstains of a hundred past conquerors.
+          across, a modern skyline awaits me
+          (boarding terminal 50–65, i shed the skin
+          that i have worn for the past twenty-eight days).
+          two tigers and i fly into the horizon.
 
-<br /><b>ii. <em>jie ju</em> / end: </b>
-i am the lover of the half-melted mango sun.
-but on the fullest moon i gulp down salted egg yolks
-devour crushed sesame and lotus paste in the dark.
-wistful, my grandfather hands me his jade elephants.
-curls of  cigar smoke bade me farewell. 
-my memories of this town are    superimposed through
-the mirage of six months of my childhood.
-how can i recognize home?
-longing and reluctant to leave, i turn towards:
-          the modern city of infinite western luxuries,
-          a (still) too-tight <em>qi pao</em> discarded on the floor,
-          the bloodstains of a hundred past conquerors.
-across, a modern skyline awaits me
-(boarding terminal 50–65, i shed the skin
-that i have worn for the past twenty-eight days).
-two tigers and i fly into the horizon
+<br /><br /><em class = "text-sm">part of "origin" collection - awarded gold medal in the 2021 scholastic art and writing awards</em>
+        </section>
 
-        </p>
+
+        <section style = {poemStyles} class="bg-white border-dotted border-black border-2 p-8 my-4">
+          
+        <div class = "underline mb-4">eight ph(r)ases of chang’e monologue <br /></div>
+
+
+        inspired by the legend of 嫦娥, the scorned goddess of the moon in chinese mythology <br />
+        I. My palace is cold.  <br />
+        II. Far away from your arrows and fire, <br />
+        III. I welcome your accusing glare. <br />
+        IV. But to tell you the truth,  <br />
+        V. I never drank that elixir out of greed.  <br />
+        VI. Did you consider why I left?  <br />
+        VII. I escaped with my youth.  <br />
+        VIII. Yet you continue to chase. 
+          
+          <br /><br /><em class = "text-sm">part of "origin" collection - awarded gold medal in the 2021 scholastic art and writing awards, gold key in regional competition</em>
+          </section>
+        
+
         </div>
     </section>
 
 
 
       <br />
+      <div class= "flex justify-center items-center" id = "socials">
       {links.map(link => (
           <code key={link.url} style={socialLinkStyles}>
             <span>
-              <a href={`${link.url}`}>
+              <a target="_blank" href={`${link.url}`}>
                 {link.text}
               </a>
             </span>
           </code>
         ))} <br /> <br/>
       </div>
+      </div>
+      
     </main>
   )
 }
